5191dc123ba181c5fad0edc1a124eab8f867fa1f
Mike Wagoner <mwwagoner@gmail.com>
2021-12-11 12:07:55 -0500

    Creates the css directory and gathers all css here

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
+import './css/App.css';
+import pixelBackpack from './images/pixel-art-backpack.png'
+
+
 
 function App() {
   return (
     <div className="App">
       <h1>MagePack</h1>
       <p>It's dangerous to go without it</p>
+      <img src={pixelBackpack} alt="A pixel-art brown backpack"></img>
     </div>
   );
 }
