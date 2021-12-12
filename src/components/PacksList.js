d3e5421d419f34cf18775e2f71d0fe5d2f691716
Mike Wagoner <mwwagoner@gmail.com>
2021-12-12 12:39:21 -0500

    Simplifies the component to just grab and print the first returned owner

--- src/components/PacksList.js
+++ src/components/PacksList.js
@@ -3,14 +3,10 @@ import React from "react";
 const PacksList = ({ packInfo }) => {
     return (
         <>
-            <h1>Packs</h1>
-            {packInfo.map((pack, key) => {
-                return (
-                    <h3>{pack.owner}'s pack</h3>
-                )
-            })}
+            <h3>Packs</h3>
+            <p>{packInfo.owner}</p>
         </>
-    );
+    )
 }
 
 export default PacksList;
