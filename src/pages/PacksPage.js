103329fc405bad8a0fbf4318d93fdc00323ba5b1
Mike Wagoner <mwwagoner@gmail.com>
2021-12-11 20:12:50 -0500

    Sets up state hooks for the PacksPage

--- src/pages/PacksPage.js
+++ src/pages/PacksPage.js
@@ -1,9 +1,25 @@
 import React, { useState, useEffect } from "react";
+import PacksList from "../components/PacksList";
+
 
 const PacksPage = () => {
 
-    return(
+    const [packInfo, setPackInfo] = useState({ owner: '', contents: []});
 
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await fetch(`/api/packs/list`);
+            const body = await result.json();
+            setPackInfo(body);
+        }
+        fetchData();
+    });
+
+    return(
+        <>
+            <h1>Packs</h1>
+            <PacksList packInfo={ packInfo }/>
+        </>
     )
 }
 
