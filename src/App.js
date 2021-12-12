f8badb76377f623ea182047fc33c6fb4bde0845c
Mike Wagoner <mwwagoner@gmail.com>
2021-12-12 12:38:25 -0500

    Adds PacksList component and route to PacksPage

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   Route,
 } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import PacksPage from './pages/PacksPage';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<HomePage />} exact/>
+          <Route path="/packs-list" element={<PacksPage />} exact />
         </Routes>
       </div>
     </Router>
