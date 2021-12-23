eba9c52b160e4e9b390b7cd30b9fcf98a5dee996
Michael Wagoner <mwwagoner@gmail.com>
2021-12-23 13:19:55 -0500

    Adds the PackContentList component

---
+++ src/components/PackContentsList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PackContentList = ({packInfo}) => {
+    return(
+        <>
+            <h3>Pack Contents</h3>
+        </>
+    )
+}
+
+export default PackContentList;
