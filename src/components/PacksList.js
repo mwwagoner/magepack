6e1877e8b984ac1198cc3e465cf89d9475b90b83
Michael Wagoner <mwwagoner@gmail.com>
2021-12-23 13:22:02 -0500

    Adds a call to the PackContentList component

--- src/components/PacksList.js
+++ src/components/PacksList.js
@@ -1,10 +1,14 @@
 import React from "react";
+import PackContentList from "./PackContentsList";
 
 const PacksList = ({ packInfo }) => {
     return(
         <>
             {packInfo.map((packOwner) => (
-                <p key={packOwner._id}>{packOwner.packName}</p>
+                <>
+                    <p key={packOwner._id}>{packOwner.packName}</p>
+                    <PackContentList packInfo={ packInfo } />
+                </>
             ))}
         </>
     )
