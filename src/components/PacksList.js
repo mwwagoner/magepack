383e81bcad69f3cd25315675d301649a4a5e25fd
Michael Wagoner <mwwagoner@gmail.com>
2021-12-22 22:08:36 -0500

    Removes all test code and sets the def for packInfo to an empty array

--- src/components/PacksList.js
+++ src/components/PacksList.js
@@ -1,32 +1,13 @@
 import React from "react";
 
 const PacksList = ({ packInfo }) => {
-    
-    // console.log(packInfo);
-    const arrayOfObjects = [
-        {owner: "Bob"},
-        {owner: "Michael"}
-    ];
-
     return(
         <>
-            {/* {packInfo.map((packOwner) => (
-                <p key={packOwner.owner}>{packOwner.owner}</p>
-            ))} */}
+            {packInfo.map((packOwner) => (
+                <p key={packOwner._id}>{packOwner.packName}</p>
+            ))}
         </>
     )
-    // const arrayOfObjects = [
-    //     { coffee: "Americano", size: "Medium" },
-    //     { coffee: "Espresso", size: "Large" },
-    // ]
-    
-    // return (
-    //     <>
-    //     {arrayOfObjects.map(({ coffee, size }) => (
-    //         <p key={coffee}>Coffee type {coffee} in a {size} size.</p>
-    //     ))}
-    //     </>
-    // )
 }
 
 export default PacksList;
